6f0253d2120635b3106e88ffc99f4dd442abba55
Suzuki Kojiro <pn.suzuki.kojiro@gmail.com>
2018-04-01 13:56:53 +0900

    fix mpm

--- vv/src/processor.hpp
+++ vv/src/processor.hpp
@@ -13,6 +13,18 @@ namespace vv
 		return x0 + (x1 - x0) * ratio;
 	}
 
+	template <class T>
+	auto invlerp(T x0, T x1, T x)
+	{
+		return (x - x0) / (x1 - x0);
+	}
+
+	template <class T>
+	auto squared(T x)
+	{
+		return x * x;
+	}
+
 	class processor
 	{
 	public:
@@ -28,6 +40,7 @@ namespace vv
 			, v4_(buffer_size + nsdf_size)
 			, v5_(buffer_size + nsdf_size)
 			, v6_(buffer_size)
+			, v7_(buffer_size / 2)
 		{
 		}
 
@@ -38,9 +51,9 @@ namespace vv
 
 			for (std::size_t i = 0; i < buffer_size; ++i)
 			{
-				auto r = static_cast<float>(i) / static_cast<float>(buffer_size);
-				auto w = 0.5f - 0.5f * std::cos(boost::math::constants::two_pi<float>() * r);
-				v2_[i] = v1_[i] * w;
+				auto r = static_cast<double>(i) / static_cast<double>(buffer_size);
+				auto w = 0.5 - 0.5 * std::cos(boost::math::constants::two_pi<double>() * r);
+				v2_[i] = v1_[i] * static_cast<float>(w);
 			}
 
 			fft_.transform(v2_.data(), v3_.data());
@@ -50,37 +63,42 @@ namespace vv
 
 			ifft_.transform(v4_.data(), v5_.data());
 
-			float m = 0.0;
+			for (std::size_t i = 0; i < buffer_size + nsdf_size; ++i)
+				v5_[i] /= static_cast<float>(buffer_size + nsdf_size);
 
-			for (std::size_t i = 0; i < buffer_size; ++i)
+			for (std::size_t i = 1; i < buffer_size; ++i)
 			{
 				auto j = buffer_size - i - 1;
+				v6_[j] = v6_[j + 1] + squared(v2_[i].real()) + squared(v2_[j].real());
+			}
 
-				auto x1 = v2_[i].real();
-				m += x1 * x1;
-
-				auto x2 = v2_[j].real();
-				m += x2 * x2;
-
-				if (m < std::numeric_limits<double>::min())
-					v6_[j] = 0.0f;
+			for (std::size_t i = 0; i < buffer_size / 2; ++i)
+			{
+				if (v6_[i] < std::numeric_limits<double>::min())
+					v7_[i] = 0.0f;
 				else
-					v6_[j] = 2.0f * v5_[j].real() / m;
+					v7_[i] = 2.0f * v5_[i].real() / v6_[i];
 			}
 
 			auto minimum_hz = 50.0;
-			auto maximum_hz = 1000.0;
+			auto maximum_hz = 300.0;
 
 			auto minimum_index = static_cast<std::size_t>(std::round(sampleRate_ / maximum_hz));
 			auto maximum_index = static_cast<std::size_t>(std::round(sampleRate_ / minimum_hz));
+
+			minimum_index = std::max<std::size_t>(minimum_index, 1);
+			minimum_index = std::min<std::size_t>(minimum_index, buffer_size / 2 - 2);
+
+			maximum_index = std::max<std::size_t>(maximum_index, 1);
+			maximum_index = std::min<std::size_t>(maximum_index, buffer_size / 2 - 2);
 
 			double maximum_value = 0.0;
 
 			for (std::size_t i = minimum_index; i < maximum_index; ++i)
 			{
-				auto p1 = v6_[i - 1];
-				auto p2 = v6_[i];
-				auto p3 = v6_[i + 1];
+				auto p1 = v7_[i - 1];
+				auto p2 = v7_[i];
+				auto p3 = v7_[i + 1];
 
 				if (p1 < p2 && p2 > p3 && p2 > maximum_value)
 					maximum_value = p2;
@@ -91,11 +109,11 @@ namespace vv
 
 			for (std::size_t i = minimum_index; i < maximum_index; ++i)
 			{
-				auto p1 = v6_[i - 1];
-				auto p2 = v6_[i];
-				auto p3 = v6_[i + 1];
+				auto p1 = v7_[i - 1];
+				auto p2 = v7_[i];
+				auto p3 = v7_[i + 1];
 
-				if (p1 < p2 && p2 > p3 && p2 > maximum_value * 0.95)
+				if (p1 < p2 && p2 > p3 && p2 > maximum_value * 0.9)
 				{
 					peak_index = i;
 					peak_value = p2;
@@ -112,8 +130,6 @@ namespace vv
 
 			if (peak_index)
 			{
-				auto virtual_peak_index = *peak_index;
-
 				std::size_t last_dst = 0;
 				std::size_t last_src1 = 0;
 				std::size_t last_src2 = 0;
@@ -166,15 +182,15 @@ namespace vv
 					last_src_ratio = src_ratio;
 				};
 
-				auto q = buffer_size / virtual_peak_index;
-				auto r = buffer_size % virtual_peak_index;
+				auto q = buffer_size / *peak_index;
+				auto r = buffer_size % *peak_index;
 
-				auto nf = (static_cast<double>(buffer_size) * pitch_shift - static_cast<double>(r)) / static_cast<double>(virtual_peak_index);
+				auto nf = (static_cast<double>(buffer_size) * pitch_shift - static_cast<double>(r)) / static_cast<double>(*peak_index);
 				auto n = static_cast<std::size_t>(std::max(0.0, std::round(nf)));
 
 				if (q != 0 && n != 0)
 				{
-					auto actual_pitch_shift = static_cast<double>(n * virtual_peak_index + r) / static_cast<double>(buffer_size);
+					auto actual_pitch_shift = static_cast<double>(n * *peak_index + r) / static_cast<double>(buffer_size);
 
 					for (std::size_t i = 1; i <= n; ++i)
 					{
@@ -185,8 +201,8 @@ namespace vv
 
 						auto frame_index = static_cast<std::size_t>(std::floor(frame_indexf));
 
-						auto dst = static_cast<std::size_t>(std::floor(static_cast<double>(i * virtual_peak_index) / actual_pitch_shift));
-						auto src = frame_index * virtual_peak_index;
+						auto dst = static_cast<std::size_t>(std::floor(static_cast<double>(i * *peak_index) / actual_pitch_shift));
+						auto src = frame_index * *peak_index;
 
 						if (frame_index == q)
 						{
@@ -195,7 +211,7 @@ namespace vv
 						else
 						{
 							auto src_ratio = frame_indexf - std::floor(frame_indexf);
-							overlap(dst, src, src + virtual_peak_index, src_ratio);
+							overlap(dst, src, src + *peak_index, src_ratio);
 						}
 					}
 
@@ -222,6 +238,7 @@ namespace vv
 		std::vector<std::complex<float>> v4_;
 		std::vector<std::complex<float>> v5_;
 		std::vector<float> v6_;
+		std::vector<float> v7_;
 
 		boost::optional<std::size_t> last_peak_index_;
 
